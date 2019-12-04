a4cb311e714932fcab8468c69a8f125d319090c8
daniel <vdmeulen.daniel@gmail.com>
2019-12-04 13:00:29 +0100

    working on improving 03

--- 2019/0301a.py
+++ 2019/0301a.py
@@ -2,16 +2,9 @@
 Advent of Code 2019, day 3, puzzle 1
 this time with a list of tuples
 """
-import collections as cl
-
-# lines = ["R8,U5,L5,D3", "U7,R6,D4,L4"]  # distance 6
-# lines = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]  # distance 159
-# lines = ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]  # distance 135
-
 lines = []
 with open("input03.txt") as f:
-    for line in f:
-        lines.append(line)
+    lines = f.readlines()
 
 
 def plot_path(path):
@@ -19,22 +12,17 @@ def plot_path(path):
     x, y = 0, 0
     directions = [(s[0], int(s[1:])) for s in path]
     for direction, steps in directions:
-        if direction == "U":
-            for i in range(1, steps + 1):
-                coords.append((x, y + i))
-            y = y + steps
-        if direction == "D":
-            for i in range(1, steps + 1):
-                coords.append((x, y - i))
-            y = y - steps
-        if direction == "R":
-            for i in range(1, steps + 1):
-                coords.append((x + i, y))
-            x = x + steps
-        if direction == "L":
-            for i in range(1, steps + 1):
-                coords.append((x - i, y))
-            x = x - steps
+        for i in range(1, steps + 1):
+            if direction == "U":
+                y += 1
+            if direction == "D":
+                y -= 1
+            if direction == "R":
+                x += 1
+            if direction == "L":
+                x -= 1
+
+            coords.append((x, y))
     return coords
 
 
@@ -43,4 +31,5 @@ wires = []
 for nr, path in enumerate(paths):
     wires.append(plot_path(path))
 
-print(min([abs(x) + abs(y) for x, y in set(wires[0]) & set(wires[1])]))
+print('answer puzzle 1:', min([abs(x) + abs(y) for x, y in set(wires[0]) & set(wires[1])]))
+print('answer puzzle 2:', min([wires[0].index(s) + wires[1].index(s) for s in set(wires[0]) & set(wires[1])]))
