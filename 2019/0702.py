9cd5ea00e39eab8a0da89b103f7cc92db0dba3b8
daniel <vdmeulen.daniel@gmail.com>
2019-12-25 01:55:56 +0100

    finished day 7

--- 2019/0702.py
+++ 2019/0702.py
@@ -1,13 +1,13 @@
 """
-Advent of Code 2019, day 7, puzzle 1
+Advent of Code 2019, day 7, puzzle 2
+Use a generator to solve this puzzle
 """
 from itertools import permutations
 
 debug = False
 with open("input07.txt") as f:
     line = f.readline()
-# line = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"  # 139629729
-# line = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10" # 18216
+
 input_codes = [int(s) for s in line.split(",")]
 
 
@@ -81,58 +81,30 @@ def intcode():
 
 
 highest_output = 0
-highest_phase = []
-phases = permutations([5, 6, 7, 8, 9], 5)
-# phases = [[9, 8, 7, 6, 5], ]
-# phases = [[9, 7, 8, 5, 6], ]
+highest_phase = None
+for phase in permutations([5, 6, 7, 8, 9], 5):
+    signal = 0
+    amps = [intcode(), intcode(), intcode(), intcode(), intcode()]
 
-for phase in phases:
-    e = 0
-    A = intcode()
-    B = intcode()
-    C = intcode()
-    D = intcode()
-    E = intcode()
-    next(A)
-    A.send(phase[0])
-    next(B)
-    B.send(phase[1])
-    next(C)
-    C.send(phase[2])
-    next(D)
-    D.send(phase[3])
-    next(E)
-    E.send(phase[4])
+    # phase initialization
+    for i, amp in enumerate(amps):
+        next(amp)
+        amp.send(phase[i])
+
+    # feedback loop
     try:
         while True:
-            a = A.send(e)
-            if debug:
-                print(f'{a=}')
-            b = B.send(a)
-            if debug:
-                print(f'{b=}')
-            c = C.send(b)
-            if debug:
-                print(f'{c=}')
-            d = D.send(c)
-            if debug:
-                print(f'{d=}')
-            e = E.send(d)
-            if debug:
-                print(f'{e=}')
+            for amp in amps:
+                signal = amp.send(signal)
 
-            if e > highest_output:
-                highest_output = e
+            if signal > highest_output:
+                highest_output = signal
                 highest_phase = phase
 
-            next(A)
-            next(B)
-            next(C)
-            next(D)
-            next(E)
+            for amp in amps:
+                next(amp)
 
     except StopIteration:
-        # print(f'{phase=} {e=}')
         pass
 
 print(f'phase {highest_phase} has output {highest_output}')
