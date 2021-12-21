b0511fd50ce775d1655c5223384afbb291c2af34
daniel (nuc) <vdmeulen.daniel@gmail.com>
2021-12-21 12:46:27 +0100

    202117

--- 2021/day17.py
+++ 2021/day17.py
@@ -1,25 +1,27 @@
 import re
-from itertools import count
-
 with open("input17.txt") as f:
     line = f.readline().strip()
+
 x1, x2, y1, y2 = map(int, re.findall(r"[-]?\d+", line))
-# x1, x2, y1, y2 = 20, 30, -10, -5
+heighest = 0
+hit = set()
+for x in range(x2 + 1):
+    for y in range(y1, abs(y1)):
+        x_distance = y_distance = 0
+        x_velocity = x
+        y_velocity = y
+        while x_distance <= x2:
+            x_distance += x_velocity
+            if x_velocity > 0:  # no negative speed
+                x_velocity -= 1
+            y_distance += y_velocity
+            heighest = max(y_distance, heighest)
+            y_velocity -= 1
+            if x1 <= x_distance <= x2 and y1 <= y_distance <= y2:
+                hit.add((x, y))
 
-hor = []
-t = 0
-for i in count():
-    t += i
-    if x1 <= t <= x2:
-        hor.append(i)
-    if t > x2:
-        break
+            if y_distance < y1:
+                break
 
-# na 6 of 7 stappen op -5 .. -10
-ver = []
-y = 162
-t = 0
-for i in range(1, 400):
-    ver.append(t + y)
-    t = t + y
-    y -= 1
+print("answer 1:", heighest)
+print("answer 2:", len(hit))
