0606e7a5cd79e11dcfd95876915ff751b8fb4764
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2022-01-25 19:48:54 +0100

    201619

--- 2016/day19.py
+++ 2016/day19.py
@@ -1,15 +1,31 @@
 from collections import deque
-elfs = 3017957
-# elfs = 5
 
-table = []
-for elf in range(1, elfs + 1):
-    table.append(elf)
 
-q = deque(table)
-while len(q) > 1:
-    q.append(q.popleft())
-    q.popleft()
+def take_from_neighbour(elfs):
+    q = deque(x for x in range(1, elfs + 1))
+    while len(q) > 1:
+        q.append(q.popleft())
+        q.popleft()
 
-print(q)
-print("done")
+    return q.pop()
+
+
+def take_from_across(elfs):
+    middle = elfs // 2 + 1
+    left = deque(x for x in range(1, middle))
+    right = deque(x for x in range(elfs + 1, middle, -1))
+
+    while left and right:
+        if len(left) > len(right):
+            left.pop()
+        else:
+            right.pop()
+
+        # rotate
+        right.appendleft(left.popleft())
+        left.append(right.pop())
+    return left[0] or right[0]
+
+
+print("answer 1:", take_from_neighbour(3_017_957))
+print("answer 2:", take_from_across(3_017_957))
