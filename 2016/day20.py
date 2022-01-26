052fef986842ac979efbd1c87904a7cb7e75ef64
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2022-01-26 12:28:36 +0100

    201620

--- 2016/day20.py
+++ 2016/day20.py
@@ -1,20 +1,22 @@
 from collections import namedtuple
-
+ips = 4_294_967_295
 rules = []
 Rule = namedtuple("Rule", "low high")
 for low, high in [line.strip().split("-") for line in open("input20.txt").readlines()]:
     rules.append(Rule(int(low), int(high)))
 rules.sort(key=lambda rule: rule.low)
-
-low = high = 0
+low = high = first = allowed = 0
 for rule in rules:
     if low < rule.low:
-        print(low)
-        break
+        if not first:
+            first = low
+        allowed += rule.low - low
+
     high = max(high, rule.high)
     low = high + 1
 
 else:
-    print(high + 1)
+    allowed += ips - min(high, ips)
 
-# 1847081 too low
+print("answer 1:", first)
+print("answer 2:", allowed)
