cfe5b24c28831738c144047875aadabd2430d74e
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-02-12 14:55:38 +0100

    201624

--- 2016/day24.py
+++ 2016/day24.py
@@ -1,12 +1,15 @@
 # the maze is 185x41 and has 8 points
+import itertools
+import math
 from collections import defaultdict, namedtuple
+from pprint import pprint
 
 point = namedtuple("Point", "x y")
 
 maze = []
 mazed = {}
 pois = {}
-with open("test24.txt") as f:
+with open("input24.txt") as f:
     for line_nr, line in enumerate(f):
         line = line.strip()
         maze_line = []
@@ -19,11 +22,13 @@ with open("test24.txt") as f:
                 mazed[point(char_nr, line_nr)] = False
             if c.isdigit():
                 pois[point(char_nr, line_nr)] = c
+                # pois[c] = point(char_nr, line_nr)
 
         maze.append(maze_line)
 
 
 def print_maze(maze, points):
+    print(f"{step=}")
     for y, line in enumerate(maze):
         for x, char in enumerate(line):
             if point(x, y) in points:
@@ -34,6 +39,7 @@ def print_maze(maze, points):
                 print("#", end="")
             else:
                 print(" ", end="")
+
         print()
 
 
@@ -46,27 +52,41 @@ def get_neighbours(neighbour: point) -> list:
     ] if not mazed[neighbour]]
 
 
-distances = defaultdict(list)
-points = [point(1, 1)]
-found = False
-step = 0
-steps = set()
-seen = set(points[0])
-while not found:
-    step += 1
-    new_points = []
-    for p in points:
-        neighbours = get_neighbours(p)
-        for neighbour in neighbours:
-            if neighbour not in seen:
-                seen.add(neighbour)
-                if neighbour in pois:
-                    # found = True
-                    distances[(1, 1)].append((pois[neighbour], step))
-                new_points.append(neighbour)
-    points = new_points
+distances = defaultdict(dict)
 
-    print_maze(maze, points)
-    print()
-    if step == 8:
-        break
+steps = set()
+for poi, nr in pois.items():
+    step = 0
+    points = [poi]
+    seen = set(points)
+    while True:
+        step += 1
+        new_points = []
+        for p in points:
+            neighbours = get_neighbours(p)
+            for neighbour in neighbours:
+                if neighbour not in seen:
+                    seen.add(neighbour)
+                    if neighbour in pois:
+                        distances[nr][pois[neighbour]] = step
+                    new_points.append(neighbour)
+        points = new_points
+        # print_maze(maze, points)
+
+        if len(distances[nr]) == len(pois) - 1:
+            break
+
+pprint(distances)
+
+routes = list(distances.keys())
+routes.remove('0')
+shortest = math.inf
+for route in itertools.permutations(routes, len(routes)):
+    start_from = '0'
+    trip_total = 0
+    for location in route:
+        trip_total += distances[start_from][location]
+        start_from = location
+    shortest = min(shortest, trip_total)
+
+print(shortest)
