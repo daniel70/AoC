7ec4394ae54cc836b4b6c6a566fb6a596ef4833d
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2022-03-15 10:48:56 +0100

    201705

--- 2017/day05.py
+++ 2017/day05.py
@@ -1,12 +1,27 @@
 with open("input05.txt") as f:
     instructions = [int(line.strip()) for line in f]
 
-counter = 0
-pointer = 0
-while 0 <= pointer < len(instructions):
-    moves = instructions[pointer]
-    instructions[pointer] += 1
+one = instructions.copy()
+two = instructions.copy()
+length = len(instructions)
+
+counter = pointer = 0
+while 0 <= pointer < length:
+    moves = one[pointer]
+    one[pointer] += 1
     pointer = pointer + moves
     counter += 1
 
 print("answer 1:", counter)
+
+counter = pointer = 0
+while 0 <= pointer < length:
+    moves = two[pointer]
+    if moves >= 3:
+        two[pointer] -= 1
+    else:
+        two[pointer] += 1
+    pointer = pointer + moves
+    counter += 1
+
+print("answer 2:", counter)
