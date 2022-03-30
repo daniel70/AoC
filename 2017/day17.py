52ffa6b522be87af212e4461b27e828f07127df3
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-03-30 16:47:16 +0200

    201717

---
+++ 2017/day17.py
@@ -0,0 +1,18 @@
+spinlocks = [0]
+steps = 349
+pos = 0
+for i in range(1, 2018):
+    pos = (pos + steps) % i + 1
+    spinlocks.insert(pos, i)
+
+print("answer 1:", spinlocks[spinlocks.index(2017) + 1])
+
+spinlocks = [0]
+pos = 0
+after_zero = 0
+for i in range(1, 50_000_000):
+    if (pos + steps) % i == 0:
+        after_zero = i
+    pos = (pos + steps) % i + 1
+
+print("answer 2:", after_zero)
