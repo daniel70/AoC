be12366c61167c96d64f54425abb68c091b37843
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-06-17 01:31:37 +0200

    201519

--- 2015/day19.py
+++ 2015/day19.py
@@ -1,33 +1,41 @@
-from collections import defaultdict
+*lines, _, molecule = open("input19.txt").readlines()
+replacements = [line.strip().split(" => ") for line in lines]
+molecule = molecule.strip()
+min_steps = None
+molecules = set()
+for before, after in replacements:
+    for position in range(len(molecule)):
+        left, right = molecule[:position], molecule[position:]
+        if right.startswith(before):
+            molecules.add(left + right.replace(before, after, 1))
 
-transformations = {}
-initials = []
-with open("input19.txt") as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            molecule = f.readline().strip()
+print("Answer 1:", len(molecules))
+
+sorted_replacements = {}  # we'll try to replace the longest strings first
+for before, after in sorted(replacements, key=lambda values: len(values[1]), reverse=True):
+    sorted_replacements[after] = before
+
+
+def find_e(s: str, step: int):
+    global min_steps
+    found = None
+    if min_steps is not None:
+        return
+
+    if s == "e":
+        min_steps = step
+        return min_steps
+
+    for left in range(len(s)):
+        if found is not None:
             break
-        l, r = line.split(" => ")
-        if l == "e":
-            initials.append(r)
-        else:
-            transformations[r] = l
+        if left > 14:  # optimized for my input, this is a magic number to me
+            return
+        for key, value in sorted_replacements.items():
+            if s[left:].startswith(key):
+                found = find_e(s.replace(key, value, 1), step=step+1)
+
+    return min_steps
 
 
-# check overlap
-# transvals = transformations.keys()
-# for k in transformations.keys():
-#     for val in transvals:
-#         if k in val and not k == val:
-#             print(f"overlap {k}, {val}")
-
-temp = molecule
-for i in range(10):
-    count = 0
-    for key, val in transformations.items():
-        if key in temp:
-            count += 1
-        temp = temp.replace(key, val, 1)
-    print(i, count)
-print(temp)
+print("Answer 2:", find_e(s=molecule, step=0))
