28e40f29d2216a354c611a96645aceb8b75e09b1
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-06-18 12:35:19 +0200

    201520

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,30 +1,29 @@
 import math
 
-goal = 33_100_000
-ten = goal // 10
-eleven = goal // 11
-
 
 def divisors(n: int) -> set[int]:
-    ds = set([1, n])
+    """
+    fastest way to get all divisors is to go from 1 to the square root
+    """
+    numbers = {1, n}
     for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
-            ds.add(i)
-            ds.add(n // i)
-    return ds
+            numbers |= {n, n // i}  # add the number and its reciprocal
+    return numbers
 
 
+goal = int(open("input20.txt").readline())
 i = 0
 found = False
 while True:
     i += 1
     nrs = divisors(i)
-    total = sum(nrs)
-    if not found and total >= ten:
+    ten = sum(nrs) * 10
+    if not found and ten >= goal:
         print("Answer 1", i)
         found = True
-    if len(nrs) <= 50 and total >= eleven:
-        print("Answer 2", i)
-        break
-
-# 928440 is too high
+    else:
+        eleven = sum(n for n in nrs if i / n <= 50) * 11
+        if eleven >= goal:
+            print("Answer 2", i)
+            break
