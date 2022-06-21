1421912fbae3e3decd539f327832d9f266094072
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-06-21 23:38:18 +0200

    201522

--- 2015/day22.py
+++ 2015/day22.py
@@ -30,27 +30,23 @@ class Spell:
     damage: int
     armor: int
     mana: int
-    immediate: bool
 
     def __str__(self):
         return self.name
 
 
 spells = {
-    "Magic Missile": Spell(name="Magic Missile", cost=53, effect=1, hp=0, damage=4, armor=0, mana=0, immediate=True),
-    "Drain": Spell(name="Drain", cost=73, effect=1, hp=2, damage=2, armor=0, mana=0, immediate=True),
-    "Shield": Spell(name="Shield", cost=113, effect=6, hp=0, damage=0, armor=7, mana=0, immediate=True),
-    "Poison": Spell(name="Poison", cost=173, effect=6, hp=0, damage=3, armor=0, mana=0, immediate=False),
-    "Recharge": Spell(name="Recharge", cost=229, effect=5, hp=0, damage=0, armor=0, mana=101, immediate=False),
+    "Magic Missile": Spell(name="Magic Missile", cost=53, effect=0, hp=0, damage=4, armor=0, mana=0),
+    "Drain": Spell(name="Drain", cost=73, effect=0, hp=2, damage=2, armor=0, mana=0),
+    "Shield": Spell(name="Shield", cost=113, effect=6, hp=0, damage=0, armor=7, mana=0),
+    "Poison": Spell(name="Poison", cost=173, effect=6, hp=0, damage=3, armor=0, mana=0),
+    "Recharge": Spell(name="Recharge", cost=229, effect=5, hp=0, damage=0, armor=0, mana=101),
 }
 
 
-def cast_spells(wizard, boss, active_spells, immediate):
-    if immediate:
-        cs = [s for s in active_spells if s.immediate == immediate]
-    else:
-        cs = active_spells
-    for spell in cs:
+def cast_spells(wizard, boss, active_spells):
+    wizard.armor = 0
+    for spell in active_spells:
         print(f"{spell.name} is dealt")
         wizard.hp += spell.hp
         wizard.armor += spell.armor
@@ -65,12 +61,15 @@ def fight(wizard: Wizard, boss: Boss, mana=0, active_spells: list[Spell] = []) -
     print('\nPlayer Turn')
     print(wizard)
     print(boss)
-    wizard, boss, active_spells = cast_spells(wizard, boss, active_spells, False)
+    wizard, boss, active_spells = cast_spells(wizard, boss, active_spells)
     spell = input(f"Choose from {','.join([spell for spell in spells if spell not in active_spells])}")
     spell = copy(spells[spell])
     print(f"Player casts {spell.name}")
-    active_spells.append(copy(spell))
-    wizard, boss, active_spells = cast_spells(wizard, boss, active_spells, True)
+    if spell.effect != 0:
+        active_spells.append(copy(spell))
+    else: # spell runs immediately
+        wizard, boss, _ = cast_spells(wizard, boss, [spell])
+
     mana += spell.cost
     wizard.mana -= spell.cost
     print(f"active spells are now {active_spells}")
@@ -78,8 +77,7 @@ def fight(wizard: Wizard, boss: Boss, mana=0, active_spells: list[Spell] = []) -
     print('\nBoss turn')
     print(wizard)
     print(boss)
-    wizard.armor = 0
-    wizard, boss, active_spells = cast_spells(wizard, boss, active_spells, False)
+    wizard, boss, active_spells = cast_spells(wizard, boss, active_spells)
     print(f"active spells are now {active_spells}")
 
     if boss.hp <= 0:
@@ -97,3 +95,5 @@ def fight(wizard: Wizard, boss: Boss, mana=0, active_spells: list[Spell] = []) -
 boss = Boss(14, 8)
 wizard = Wizard(10, 0, 250)
 ret = fight(wizard, boss)
+
+# 641 mana is the cheapest yet
