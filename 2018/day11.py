8a1cb0fff7a2699d88f48fc08c7879548a733980
daniel (nuc) <vdmeulen.daniel@gmail.com>
2022-10-11 17:34:38 +0200

    201811

--- 2018/day11.py
+++ 2018/day11.py
@@ -1,59 +1,52 @@
 from sys import maxsize
-from datetime import datetime as dt
 
 serial = 3613
-columns, rows = 300, 300
-cells = {}
+grid_size = 300
+grid = {}
+memoize = {}
 
 
-def get_cell(x, y):
-    return 300 * (y - 1) + x
-
-
-def get_grid_cells(x, y, size):
-    top_left = get_cell(x, y)
-    grid_cells = []
-    for dx in range(0, size):
-        for dy in range(0, size):
-            grid_cells.append(top_left + dx + (dy * columns))
-    return grid_cells
-
-
-def power(x, y, serial):
+def get_power(x, y, serial):
     rack_id = x + 10
     return ((rack_id * y + serial) * rack_id) % 1000 // 100 - 5
 
 
-
-for x in range(1, columns + 1):
-    for y in range(1, rows + 1):
-        cell = get_cell(x, y)
-        cells[cell] = power(x, y, serial)
-
-
 def highest_grid(max_size):
-    previous_highest = 0
     sizes = {}
     for size in range(1, max_size + 1):
+        max_power = -maxsize
+        cell_with_max_power = None
+        for x in range(1, grid_size - size + 2):
+            for y in range(1, grid_size - size + 2):
+                power = 0
+                # get the right and bottom values of the grid
+                for dy in range(0, size - 1):
+                    power += grid[(x + size - 1, y + dy)]
+                for dx in range(0, size):
+                    power += grid[(x + dx, y + size - 1)]
 
-        total = -maxsize
-        highest = None
-        for x in range(1, columns - size):
-            for y in range(1, rows - size):
-                square = 0
-                for cell in get_grid_cells(x, y, size):
-                    square += cells[cell]
+                # add to that the power of the inner grid
+                if size > 1:
+                    power += memoize[(x, y, size - 1)]
+                # and store it for the next grid
+                memoize[(x, y, size)] = power
 
-                if square > total:
-                    total = square
-                    highest = (x, y, total)
+                if power > max_power:
+                    max_power = power
+                    cell_with_max_power = (x, y, max_power)
 
-        sizes[size] = (highest)
-
+        sizes[size] = cell_with_max_power
     return sizes
 
 
-tick = dt.now()
-sizes = highest_grid(30)
-tock = dt.now()
-print((tock - tick).total_seconds())
+for x in range(1, grid_size + 1):
+    for y in range(1, grid_size + 1):
+        grid[(x, y)] = get_power(x, y, serial)
+
+
+sizes = highest_grid(300)
+print("answer 1:", ",".join([str(x) for x in sizes[3][:2]]))
+nr, cell = max(sizes.items(), key=lambda k: k[1][2])
+answer2 = list(cell[:2])
+answer2.append(nr)
+print("answer 2:", ",".join([str(x) for x in answer2]))
