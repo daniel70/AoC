b61a45defbdc3ddbf8ebe5b6f683ced36c6420af
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2022-12-13 02:00:44 +0100

    202210

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,26 +1,24 @@
-registers = {
-    "X": [1]
-}
+X = [1]
 current = 1
 
 with open("input10.txt") as f:
     for line in f:
         line = line.strip()
         if line == "noop":
-            registers["X"].append(current)
+            X.append(current)
             continue
         op, val = line.split()
         if op == "addx":
-            registers["X"].append(current)
+            X.append(current)
             current += int(val)
-            registers["X"].append(current)
+            X.append(current)
 
 total = 0
 for x in [20, 60, 100, 140, 180, 220]:
-    total += registers["X"][x-1] * x
+    total += X[x-1] * x
 print("answer 1:", total)
 
-for cycle, pos in enumerate(registers["X"][:-1]):
+for cycle, pos in enumerate(X[:-1]):
     if pos - 1 <= cycle % 40 <= pos + 1:
         print("#", end="")
     else:
