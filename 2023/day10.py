7659d1d23bef46405b3cbd9ca9db5e21dab65a76
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2023-12-25 04:30:22 +0100

    202310 solved

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,8 +1,8 @@
 maze: list[list[str]] = []
 with open("input10.txt") as file:
-    for line in file:
-        line = line.strip()
-        maze.append(list(line))
+    for previous in file:
+        previous = previous.strip()
+        maze.append(list(previous))
 
 directions = {
     "|": {"N", "S"},
@@ -99,196 +99,28 @@ while not is_seen:
 print(S)
 print(steps)
 
-
-# S and N = F|L and 7|J
-# E and W = F-7 and L-J
-# find all the outsiders, count what's left
-
-
-def is_inside(point: tuple[int, int], seen, maze) -> bool:
-    if point in seen:
-        # print("seen")
-        return False
-
-    px, py = point
-    pipes = {maze[x][y] for x, y in seen if x < px and y == py}  # north
-    if not pipes - {"F", "|", "L"} or not pipes - {"7", "|", "J"}:
-        # print("north")
-        return False
-    pipes = {maze[x][y] for x, y in seen if x > px and y == py}  # south
-    if not pipes - {"F", "|", "L"} or not pipes - {"7", "|", "J"}:
-        # print("south")
-        return False
-    pipes = {maze[x][y] for x, y in seen if x == px and y > py}  # east
-    if not pipes - {"F", "-", "7"} or not pipes - {"L", "-", "J"}:
-        # print("east")
-        return False
-    pipes = {maze[x][y] for x, y in seen if x == px and y < py}  # west
-    if not pipes - {"F", "-", "7"} or not pipes - {"L", "-", "J"}:
-        # print("west")
-        return False
-
-    return True
-
-
-def has_outside_neighbour(point, seen, maze, outsides):
-    if point in seen:
-        return False
-    if point in outsides:
-        return False
-
-    x, y = point
-    if (
-        (x + 1, y) in outsides
-        or (x - 1, y) in outsides
-        or (x, y + 1) in outsides
-        or (x, y - 1) in outsides
-
-        # or (x + 1, y + 1) in outsides
-        # or (x + 1, y - 1) in outsides
-        # or (x - 1, y + 1) in outsides
-        # or (x - 1, y - 1) in outsides
-
-    ):
-        return True
-
-    return False
-
-def has_outside_in_line(point: tuple[int, int], seen, outsides, maze) -> bool:
-    if point in seen:
-        return False
-    if point in outsides:
-        return False
-
-    px, py = point
-    pipes = set()
-    for x in range(px - 1, 0, -1):
-        if (x, py) in seen:
-            pipes.add(maze[x][py])
-            if pipes - {"F", "|", "L"} and pipes - {"7", "|", "J"}:
-                break
-        if (x, py) in outsides:
-            return True
-
-    pipes = set()
-    for x in range(px + 1, len(maze), 1):
-        if (x, py) in seen:
-            pipes.add(maze[x][py])
-            if pipes - {"F", "|", "L"} and pipes - {"7", "|", "J"}:
-                break
-        if (x, py) in outsides:
-            return True
-
-    pipes = set()
-    for y in range(py + 1, len(maze[0]), 1):
-        if (px, y) in seen:
-            pipes.add(maze[px][y])
-            if pipes - {"F", "-", "7"} and pipes - {"L", "-", "J"}:
-                break
-        if (px, y) in outsides:
-            return True
-
-    pipes = set()
-    for y in range(py - 1, 0, -1):
-        if (px, y) in seen:
-            pipes.add(maze[px][y])
-            if pipes - {"F", "-", "7"} and pipes - {"L", "-", "J"}:
-                break
-        if (px, y) in outsides:
-            return True
-
-    return False
-
-
-outsides = []
-insides = []
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if (x, y) not in seen and is_inside((x, y), seen, maze) == False:
-            outsides.append((x, y))
-
-
-found = True
-while found:
-    found = False
-    for x in range(len(maze)):
-        for y in range(len(maze[0])):
-            if has_outside_neighbour((x, y), seen, maze, outsides):
-                outsides.append((x, y))
-                found = True
-
-# find outsides in straight lines
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if has_outside_in_line((x, y), seen, outsides, maze):
-            print("found one")
-            outsides.append((x, y))
-
-
-found = True
-while found:
-    found = False
-    for x in range(len(maze)):
-        for y in range(len(maze[0])):
-            if has_outside_neighbour((x, y), seen, maze, outsides):
-                outsides.append((x, y))
-                found = True
-
-
-
-# find outsides in straight lines
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if has_outside_in_line((x, y), seen, outsides, maze):
-            print("found one")
-            outsides.append((x, y))
-
-
-found = True
-while found:
-    found = False
-    for x in range(len(maze)):
-        for y in range(len(maze[0])):
-            if has_outside_neighbour((x, y), seen, maze, outsides):
-                outsides.append((x, y))
-                found = True
-
-# find outsides in straight lines
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if has_outside_in_line((x, y), seen, outsides, maze):
-            print("found one")
-            outsides.append((x, y))
-
-
-found = True
-while found:
-    found = False
-    for x in range(len(maze)):
-        for y in range(len(maze[0])):
-            if has_outside_neighbour((x, y), seen, maze, outsides):
-                outsides.append((x, y))
-                found = True
-
-insides = []
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if (x, y) not in seen and (x, y) not in outsides:
-            insides.append((x, y))
-
-print(len(insides))
-
-for x in range(len(maze)):
-    for y in range(len(maze[0])):
-        if (x, y) in outsides:
-            print("O", end="")
-        elif (x, y) in insides:
-            print("I", end="")
-        elif (x, y) in seen:
-            print(maze[x][y], end="")
+inside = []
+outside = []
+for y, row in enumerate(maze):
+    previous = ""
+    count = 0
+    for x, col in enumerate(row):
+        if (y, x) not in seen:
+            if count % 2 == 0:
+                outside.append((y, x))
+            else:
+                inside.append((y, x))
         else:
-            print("?", end="")
-    print("")
+            if maze[y][x] == "|":
+                count += 1
+            elif maze[y][x] in ["L", "F"]:
+                previous = maze[y][x]
+            elif maze[y][x] == "7" and previous == "L":
+                count += 1
+                previous = ""
+            elif maze[y][x] == "J" and previous == "F":
+                count += 1
+                previous = ""
 
+print(len(inside))
 # 819 too high, 815 is incorrect, 811
-
