5d36283aca945f893514faa22d778c1993afecbc
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2024-12-03 15:53:07 +0100

    part 2 completed

--- 2024/day02.py
+++ 2024/day02.py
@@ -1,58 +1,34 @@
 reports = []
-lines = open(r".\2024\test02bad.txt").read().strip().split("\n")
+answer1 = answer2 = 0
+
+lines = open(r".\2024\input02.txt").read().strip().split("\n")
 for line in lines:
     reports.append([int(level) for level in line.split()])
 
-answer1 = 0
-answer2 = 0
-
-def save_report(report) -> int:
+def save_report(report) -> bool:
     """
     If the report is save -> return 0
     else return the number of the offending level
     """
     up = report[0] < report[1]
-    index = 0
     for left, right in zip(report, report[1:]):
-        index += 1
         if up and not (left < right <= left + 3):
-            return index
+            return False
         elif not up and not (left > right >= left - 3):
-            return index
+            return False
     
-    return 0
+    return True
 
-# def safe2(report):
-#     return any(save_report(reports[:i] + reports[i + 1:]) for i in range(len(reports)))
 
-# print(sum(safe2(reports) for reports in lines))
-
-for i, report in enumerate(reports):
-    print(i, report, end=" ")
-    result = save_report(report)
-    if result == 0:
+for report in reports:
+    if save_report(report):
         answer1 += 1
         answer2 += 1
-        print("Safe without removing any level")
-        continue
-    result_without_first_element = save_report(report[1:])
-    if result_without_first_element == 0:
-        answer2 += 1
-        print("Safe by removal of first element")
-        continue
-    popped = report.pop(result)
-    popped_index = result
-    # print(f"Removing {popped}")
-    result = save_report(report)
-    if result == 0:
-        answer2 += 1
-        print(f"Safe after removal of {popped} (idx={popped_index})")
-        continue
-
-    print(f"Bad idx1:{popped_index} idx2:{result}")
+    else:
+        for dampener in [report[:i] + report[i+1:] for i in range(len(report))]:
+            if save_report(dampener):
+                answer2 += 1
+                break
 
 print(answer1)
 print(answer2)
-# 323 too low, because error in 0
-# 611 too high
-# 340 too low
