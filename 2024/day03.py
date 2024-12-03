cab635b0c8e09a94e89ce7a590e14d4a3bf3deb7
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2024-12-04 00:24:13 +0100

    match casing day 3

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,21 +1,19 @@
 import re
-mul = re.compile(r"(don\'t|mul\((\d{1,3}),(\d{1,3})\)|do)")
+mul = re.compile(r"(don\'t|do|mul\((\d{1,3}),(\d{1,3})\))")
 answer1 = answer2 = 0
 data = open(r".\2024\input03.txt").read().strip().split("\n")
 do = True
 for line in data:
-    matches = mul.findall(line)
-    for match in matches:
-        instruction, left, right = match
-        if instruction == "do":
-            do = True
-        elif instruction == "don't":
-            do = False
-        else:
-            temp = (int(left) * int(right))
-            answer1 += temp
-            answer2 += temp if do else 0
-
+    for finding in mul.findall(line):
+        match finding:
+            case "do", _, _:
+                do = True
+            case "don't", _, _:
+                do = False
+            case _, left, right:
+                temp = (int(left) * int(right))
+                answer1 += temp
+                answer2 += temp if do else 0
 
 print("answer 1: ", answer1)
 print("answer 2: ", answer2)
