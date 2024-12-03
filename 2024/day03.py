2dcdb9fe3f6d51f09007602e79a902b1ed9f92cb
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2024-12-03 10:15:05 +0100

    second

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,14 +1,21 @@
 import re
-mul = re.compile(r"mul\((\d+),(\d+)\)")
-total = 0
+mul = re.compile(r"(don\'t|mul\((\d{1,3}),(\d{1,3})\)|do)")
+answer1 = answer2 = 0
 data = open(r".\2024\input03.txt").read().strip().split("\n")
+do = True
 for line in data:
     matches = mul.findall(line)
-    if matches is None:
-        print("nothing here")
-        continue
-    for left, right in matches:
-        total += int(left) * int(right)
+    for match in matches:
+        instruction, left, right = match
+        if instruction == "do":
+            do = True
+        elif instruction == "don't":
+            do = False
+        else:
+            temp = (int(left) * int(right))
+            answer1 += temp
+            answer2 += temp if do else 0
 
 
-print(total)
+print("answer 1: ", answer1)
+print("answer 2: ", answer2)
