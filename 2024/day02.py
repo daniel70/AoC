6364874178e75fcec1795017395d3c180c13abb0
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-05 01:38:54 +0100

    minor changes

--- 2024/day02.py
+++ 2024/day02.py
@@ -5,30 +5,23 @@ lines = open(r"input02.txt").read().strip().split("\n")
 for line in lines:
     reports.append([int(level) for level in line.split()])
 
-def save_report(report) -> bool:
-    """
-    If the report is save -> return 0
-    else return the number of the offending level
-    """
+def is_safe(report) -> bool:
     up = report[0] < report[1]
     for left, right in zip(report, report[1:]):
         if up and not (left < right <= left + 3):
             return False
         elif not up and not (left > right >= left - 3):
             return False
-    
     return True
 
 
 for report in reports:
-    if save_report(report):
+    if is_safe(report):
         answer1 += 1
-        answer2 += 1
-    else:
-        for dampener in [report[:i] + report[i+1:] for i in range(len(report))]:
-            if save_report(dampener):
-                answer2 += 1
-                break
+    for dampener in [report[:i] + report[i+1:] for i in range(len(report))]:
+        if is_safe(dampener):
+            answer2 += 1
+            break
 
 print("answer 1: ", answer1)
 print("answer 2: ", answer2)
