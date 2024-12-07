f11531b3586a771529010b2bab602db41dec3adc
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-07 23:32:03 +0100

    day 7 part 2

--- 2024/day07.py
+++ 2024/day07.py
@@ -1,26 +1,29 @@
 import itertools
 import operator
 
-hits = set()
+def concat(a, b):
+    return int(f"{a}{b}")
+
+
+answer1 = set()
+answer2 = set()
 equations = dict()
+ops = [operator.add, operator.mul, concat]
+
 for line in open('input07.txt').readlines():
     left, right = line.split(':')
     equations[int(left)] = [int(x) for x in right.split()]
 
-
-def concat(a, b):
-    return int(f"{a}{b}")
-
-ops = [operator.add, operator.mul, concat]
-
 for k, v in equations.items():
-    found = False
     for attempt in itertools.product(ops, repeat=len(v)-1):
         temp = v[0]
         for op, val in zip(attempt, v[1:]):
             temp = op(temp, val)
         if temp == k:
-            found = True
-            hits.add(k)
+            if concat not in attempt:
+                answer1.add(k)
+            answer2.add(k)
 
-print(sum(hits))
+
+print(sum(answer1))
+print(sum(answer2))
