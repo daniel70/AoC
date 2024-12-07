219759ed23e08319a2b8d7f36512ba1344484c47
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-07 17:30:53 +0100

    day 7 part 1 solved

---
+++ 2024/day07.py
@@ -0,0 +1,22 @@
+import itertools
+import operator
+
+hits = set()
+equations = dict()
+for line in open('input07.txt').readlines():
+    left, right = line.split(':')
+    equations[int(left)] = [int(x) for x in right.split()]
+
+ops = [operator.add, operator.mul]
+
+for k, v in equations.items():
+    found = False
+    for attempt in itertools.product(ops, repeat=len(v)-1):
+        temp = v[0]
+        for op, val in zip(attempt, v[1:]):
+            temp = op(temp, val)
+        if temp == k:
+            found = True
+            hits.add(k)
+
+print(sum(hits))
