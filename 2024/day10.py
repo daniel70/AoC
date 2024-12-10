313b57d3a718215a468c0b93c6c1c1b1557a891a
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-10 14:26:43 +0100

    day 10 part 1

---
+++ 2024/day10.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+aria = {(i + j * 1j): int(c) for i, line in enumerate(open("input10.txt").readlines()) for j, c in enumerate(line.strip())}
+seen = {step: [] for step in range(10)}
+seen[0] = ([[pos,] for pos, height in aria.items() if height == 0])
+
+for round in range(10):
+    for path in seen[round]:
+        for dir in [-1, 1j, 1, -1j]:
+            if aria.get(path[-1] + dir) == round + 1:
+                seen[round+1].append(path + [path[-1] + dir])
+
+answer1 = len({(path[0], path[-1]) for path in seen[9]})
+print(answer1)
