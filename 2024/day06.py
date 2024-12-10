ac9c275252ea7d1b4207fa51a238e53c7afb9a4f
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-10 01:23:06 +0100

    day 6 part 2 finally and with complex numbers

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,88 +1,24 @@
-from collections import defaultdict
-from itertools import cycle
+lab = {}
+for i, line in enumerate(open('input06.txt').readlines()):
+    for j, char in enumerate(line.strip()):
+        lab[i + j * 1j] = char
+        if char == "^":
+            start = i + j * 1j
 
-maze = defaultdict(str) | {(x, y): s for y, line in enumerate(open("input06.txt").readlines())
-                                     for x, s in enumerate(line.strip())}
 
-pos = [k for k, v in maze.items() if v == '^']
-x, y = pos[0]
-maze[(x, y)] = '.'
-char = '.'
-direction = 'N'
-seen = set()
-seenl = []
-seend = defaultdict(list)
-obstructions = set()
-
-def get_next_pos(direction, position) -> tuple:
-    x, y = position
-    if direction == 'N':
-        return x, y - 1
-    elif direction == 'E':
-        return x + 1, y
-    elif direction == 'S':
-        return x, y + 1
-    elif direction == 'W':
-        return x - 1, y
-
-def turn(direction):
-    return 'NESW'[('NESW'.index(direction) + 1) % 4]
-
-def is_loop(direction, position) -> bool:
-    seen_this_loop = defaultdict(list)
-    # first turn 90 degrees then start walking
-    direction = turn(direction)
-    x, y = position
-    char = maze[(x, y)]
-    while True:
-        next_pos = get_next_pos(direction, (x, y))
-        next_char = maze[next_pos]
-
-        if next_char == '':
-            return False
-
-        if next_char == '#':
-            direction = turn(direction)
+def walk(lab):
+    dir, pos, seen = -1, start, set()
+    while pos in lab and (pos, dir) not in seen:
+        seen |= {(pos, dir)}
+        if lab.get(pos+dir) == "#":
+            dir *= -1j # rotate 90º
             continue
+        pos += dir
+    return pos in lab, {pos for pos, dir in seen}
 
-        x, y = next_pos
+is_loop, path = walk(lab)
+answer1 = len(path)
+answer2 = sum(walk(lab | {pos: "#"})[0] for pos in path)
 
-        if direction in seend[(x, y)]:
-            return True
-
-        if direction in seen_this_loop[(x, y)]:
-            print("loop closed")
-            return True
-
-        seen_this_loop[(x, y)].append(direction)
-
-
-while True:
-    next_pos = get_next_pos(direction, (x, y))
-    next_char = maze[next_pos]
-
-    if next_char == '':
-        seen.add((x, y))
-        break
-
-    if next_char == '#':
-        direction = turn(direction)
-        continue
-
-    if is_loop(direction, (x, y)):
-        obstructions.add(next_pos)
-
-    seen.add((x, y))
-    seenl.append((x, y))
-    seend[(x, y)].append(direction)
-    x, y = next_pos
-
-
-print(len(seen))
-print(len(obstructions))
-
-# 1678 is too low
-# 1818 is too high
-
-
-
+print("answer 1:", answer1)
+print("answer 2:", answer2)
