0b6d21011d7ce6ec774f90a20e7cd961ccae9406
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2024-12-12 14:35:04 +0100

    finished part 2

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,18 +1,23 @@
-from collections import deque
+from collections import deque, Counter
 
-stones = deque(map(int, open(r"2024\input11.txt").read().split()))
-stones = deque([0])
-for r in range(12):
-    new = deque()
+
+totals = Counter(map(int, open(r"2024\input11.txt").read().split()))
+stones = deque(totals.keys())
+previous = totals.copy()
+for r in range(75):
+    new = Counter()
     while stones:
         stone = stones.popleft()
         if stone == 0:
-            new.append(1)
+            new[1] += previous[stone]
         elif len(str(stone)) % 2 == 0:
             left, right = int(str(stone)[:len(str(stone))//2]), int(str(stone)[len(str(stone))//2:])
-            new.extend([left, right])
+            new[left] += previous[stone]
+            new[right] += previous[stone]
         else:
-            new.append(stone * 2024)
-    stones = new
-    print(f"round {r}:", len(stones), stones)
-print(len(stones))
+            new[stone * 2024] += previous[stone]
+    totals.update(new)
+    previous = new.copy()
+    stones = deque(new.keys())
+    print(f"round {r+1}:", sum(new.values()))
+print(sum(totals.values()))
