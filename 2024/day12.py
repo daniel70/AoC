efb461e217188e61bb2c9158ca8ea0abb2268eb1
Daniel van der Meulen <daniel.van.der.meulen@ing.com>
2024-12-13 00:26:43 +0100

    pretty

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,19 +1,12 @@
 garden = { i+j*1j:c for i, r in enumerate(open(r"2024\input12.txt").readlines())
                     for j, c in enumerate(r.strip())}
-seen = {}
 
 def walk(pos, plant, region):
     if pos in seen or garden[pos] != plant:
         return
     
     region.append(pos)
-    
-    # how many sides do I have?
-    sides = 4
-    for neighbour in [pos + dir for dir in [-1, 1, -1j, 1j]]:
-        if neighbour in garden and garden[neighbour] == plant:
-            sides -= 1
-        seen[pos] = sides
+    seen.add(pos)
     
     # call the neighbours
     for neighbour in [pos + dir for dir in [-1, 1, -1j, 1j]]:
@@ -21,41 +14,36 @@ def walk(pos, plant, region):
             walk(neighbour, plant, region)
 
 
-regions = []
+seen, regions = set(), list()
 for k, v in garden.items():
     if k not in seen:
         regions.append([])
         walk(k, v, regions[-1])
 
-answer1 = []
+answer1 = 0
+answer2 = 0
 for region in regions:
-    parameter = 0
-    for pos in region:
-        parameter += seen[pos]
-    answer1.append(parameter * len(region))
-
-answer2 = []
-for region in regions:
-    parameter = 0
     for pos in region:
         # top
         if pos-1 not in region:
+            answer1 += len(region)
             if pos-1j not in region or pos-1-1j in region:
-                parameter += 1
+                answer2 += len(region)
         # bottom
         if pos+1 not in region:
+            answer1 += len(region)
             if pos-1j not in region or pos+1-1j in region:
-                parameter += 1
+                answer2 += len(region)
         # left
         if pos-1j not in region:
+            answer1 += len(region)
             if pos-1 not in region or pos-1-1j in region:
-                parameter += 1
+                answer2 += len(region)
         # right
         if pos+1j not in region:
+            answer1 += len(region)
             if pos-1 not in region or pos-1+1j in region:
-                parameter += 1
-    answer2.append(parameter * len(region))
+                answer2 += len(region)
 
-
-print(sum(answer1))
-print(sum(answer2))
+print(answer1)
+print(answer2)
