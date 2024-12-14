d07be2270e9091d6507732ac0b1d56afe38c944c
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2024-12-14 16:29:01 +0100

    day 13 is working

--- 2024/day13.py
+++ 2024/day13.py
@@ -1,71 +1,26 @@
-import itertools
 import re
+
 nrs = re.compile(r'\d+')
-ax=94
-ay=34
-bx=22
-by=67
-x=X=8400
-y=Y=5400
+input = [nrs.findall(line) for line in open('input13.txt').read().split('\n\n')]
+answer1 = answer2 = 0
 
-ax=26
-ay=66
-bx=67
-by=21
-x=X=12748
-y=Y=12176
+def wins(ax, ay, bx, by, x, y, part=1) -> tuple[int, int] | None:
+    if part == 2:
+        x += 10_000_000_000_000
+        y += 10_000_000_000_000
+    b = (x * ay - y * ax) / (bx * ay - by * ax)
+    if b % 1 != 0:
+        return
+    a = (x - bx * b) / ax
+    return int(a), int(b)
 
-ax=17
-ay=86
-bx=84
-by=37
-x=X=7870
-y=Y=6450
+for part in [1, 2]:
+    for machine in input:
+        win = wins(*[int(i) for i in machine], part=part)
+        if win:
+            a, b = win
+            answer1 += a * 3 + b if part == 1 else 0
+            answer2 += a * 3 + b if part == 2 else 0
 
-ax=69
-ay=23
-bx=27
-by=71
-x=X=18641
-y=Y=10279
-
-
-
-input = [nrs.findall(line) for line in open('test13.txt').read().split('\n\n')]
-
-def wins(ax, ay, bx, by, x, y) -> list[tuple[int, int]]:
-    ret = []
-    round = 0
-    while x >= 0 and y >= 0:
-        if x % bx == 0 and y % by == 0 and x // bx == y // by:
-            ret.append((round, x // bx))
-
-        round += 1
-        x -= ax
-        y -= ay
-    return ret
-
-def big_wins(ax, ay, bx, by, x, y) -> list[tuple[int, int]]:
-    x += 10_000_000_000_000
-    y += 10_000_000_000_000
-    ret = []
-    round = 0
-    while x >= 0 and y >= 0:
-        if x % bx == 0 and y % by == 0 and x // bx == y // by:
-            ret.append((round, x // bx))
-
-        round += 1
-        x -= ax
-        y -= ay
-    return ret
-
-answer1 = 0
-for machine in input:
-    win = wins(*[int(i) for i in machine])
-    big_win = big_wins(*[int(i) for i in machine])
-    print(win)
-    print(big_win)
-    if win:
-        a, b = win[0]
-        answer1 += a * 3 + b
-print(answer1)
+print("answer 1:", answer1)
+print("answer 2:", answer2)
