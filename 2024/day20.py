fc31f1aca21194fd4d474ba9520dc296c3c923e4
Daniel van der Meulen <vdmeulen.daniel@gmail.com>
2025-01-11 17:56:46 +0100

    day 20 part 1, faster

--- 2024/day20.py
+++ 2024/day20.py
@@ -1,13 +1,14 @@
+import time
 from collections import Counter
 
 track = { i+1j*j: c for i, line in enumerate(open("input20.txt").read().splitlines())
                     for j, c in enumerate(line)}
 
+t = time.monotonic()
 start = [k for k, v in track.items() if v == 'S'][0]
 end = [k for k, v in track.items() if v == 'E'][0]
 
 def distance(pos, track, cheat=None):
-    print("let's go")
     if cheat:
         track[cheat] = "."
 
@@ -24,7 +25,7 @@ def distance(pos, track, cheat=None):
 
 def solve(pos, track):
     seen = set()
-    todo = [(pos, ())]
+    todo = [(pos, (pos,))]
     for pos, path in todo:
         if pos == end:
             return path
@@ -34,18 +35,28 @@ def solve(pos, track):
                 todo.append((new, path + (new,)))
                 seen.add(new)
 
-# track[end+1j] = '.'
-# track[(1+8j)] = '.'
 path = solve(start, track)
 
-# voor iedere stap, probeer een cheat te vinden en los de puzzel opnieuw op
-cheats = set()
-for pos in path:
+cheats = {}
+for i, pos in enumerate(path):
     for p1, p2 in ((pos+1, pos+2), (pos-1, pos-2), (pos+1j, pos+2j), (pos-1j, pos-2j)):
         if p2 in track and track[p1] == "#" and track[p2] in [".", "E", "S"]:
-            cheats.add((p1, p2))
+            cheats[(p1, p2)] = i
 
-cheats = set([c1 for c1, c2 in cheats])
-saves = {cheat: distance(start, track.copy(), cheat) for cheat in cheats}
-c = Counter([len(path) - len(save) for save in saves.values()])
-print("answer 1:", len([save for save in saves.values() if len(path) - save >= 100]))
+# cheats = set([c1 for c1, c2 in cheats])
+# saves = {cheat: distance(start, track.copy(), cheat) for cheat, cheato in cheats}
+saves = {}
+for k, path_idx in cheats.items():
+    p1, p2 = k
+    if p2 not in path:
+        print(f"{p2=} not in path")
+        continue
+    saves[p1] = path_idx - path.index(p2) - 2
+
+
+
+c = Counter([len(path) - save for save in saves.values()])
+print("answer 1:", len([save for save in saves.values() if save >= 100]))
+# print("answer 1:", len([save for save in saves.values() if len(path) - save >= 100]))
+print(c)
+print("duration:", time.monotonic() - t)
